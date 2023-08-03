ec8be9195b86eac04d3a5a64f511df8ef62be502
Gokul Sujan <gokulsujan03@gmail.com>
2023-08-03 14:58:03 +0530

    Credential straucture created for binding the login datas from the form and a function loginHandler created for verifying the login datas and handling the login

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func loginHandler(c *gin.Context) {
+	var cred Credentials
+	if err := c.ShouldBindJSON(&cred); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSOn Data"})
+		return
+	}
+
+	if cred.Username == "superadmin" && cred.Password == "superpassword" {
+		c.JSON(http.StatusOK, gin.H{"message": "Login Sucessfull"})
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Credentials"})
+	}
+}
+
 func showLoginPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", nil)
 }
