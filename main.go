32bcee683ce5ac38ad417a3f2da53b458b321c99
Gokul Sujan <gokulsujan03@gmail.com>
2023-08-03 14:46:51 +0530

    Created a handler function for the login page

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+func showLoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
 
 func main() {
 	r := gin.Default()
